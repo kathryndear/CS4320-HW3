607c6258a4d3bb00cf679108cb3e9d44a71afb19
Chris <yoo.chris2@gmail.com>
2016-03-23 17:00:22 -0400

    merged branch lossless into master

--- src/ClosureTest.java
+++ src/ClosureTest.java
@@ -5,56 +5,56 @@ import org.junit.*;
 public class ClosureTest {
 	
 	public void test_basic() {
-//		AttributeSet t1 = new AttributeSet();
-//		AttributeSet t2 = new AttributeSet();
-//		AttributeSet t3 = new AttributeSet();
-//		Set<FunctionalDependency> fds = new HashSet<FunctionalDependency>();
-//		
-//		t1.add(new Attribute("a"));
-//		t2.add(new Attribute("b"));
-//		t3.add(new Attribute("b"));
-//		t3.add(new Attribute("c"));
-//
-//		fds.add(new FunctionalDependency(t1, new Attribute("a")));
-//		fds.add(new FunctionalDependency(t1, new Attribute("b")));
-//		fds.add(new FunctionalDependency(t2, new Attribute("c")));
-//		fds.add(new FunctionalDependency(t3, new Attribute("d")));
-//		
-//		AttributeSet temp = new AttributeSet();
-//		temp = FDChecker.closure(t1, fds);
-//		for (Attribute att : temp) {
-//			System.out.println(att.name);
-//		}
+		AttributeSet t1 = new AttributeSet();
+		AttributeSet t2 = new AttributeSet();
+		AttributeSet t3 = new AttributeSet();
+		Set<FunctionalDependency> fds = new HashSet<FunctionalDependency>();
+		
+		t1.add(new Attribute("a"));
+		t2.add(new Attribute("b"));
+		t3.add(new Attribute("b"));
+		t3.add(new Attribute("c"));
+
+		fds.add(new FunctionalDependency(t1, new Attribute("a")));
+		fds.add(new FunctionalDependency(t1, new Attribute("b")));
+		fds.add(new FunctionalDependency(t2, new Attribute("c")));
+		fds.add(new FunctionalDependency(t3, new Attribute("d")));
+		
+		AttributeSet temp = new AttributeSet();
+		temp = FDChecker.closure(t1, fds);
+		for (Attribute att : temp) {
+			System.out.println(att.name);
+		}
 		
 	}
 	
 	public void test() {
-//		AttributeSet t1 = new AttributeSet();
-//		AttributeSet t2 = new AttributeSet();
-//		
-//		AttributeSet fd1 = new AttributeSet();
-//		fd1.add(new Attribute("a"));
-//		AttributeSet fd2 = new AttributeSet();
-//		fd2.add(new Attribute("c"));
-//		fd2.add(new Attribute("d"));
-//		AttributeSet fd3 = new AttributeSet();
-//		fd3.add(new Attribute("a"));
-//		fd3.add(new Attribute("c"));
-//		
-//		Set<FunctionalDependency> fds = new HashSet<FunctionalDependency>();
-//		
-//		t1.add(new Attribute("a"));
-//		t1.add(new Attribute("b"));
-//		t1.add(new Attribute("d"));
-//		t2.add(new Attribute("c"));
-//		t2.add(new Attribute("e"));
-//		t2.add(new Attribute("f"));
-//
-//		fds.add(new FunctionalDependency(fd1, new Attribute("b")));
-//		fds.add(new FunctionalDependency(fd2, new Attribute("e")));
-//		fds.add(new FunctionalDependency(fd3, new Attribute("f")));
-//		
-//		FDChecker.checkLossless(t1, t2, fds);
+		AttributeSet t1 = new AttributeSet();
+		AttributeSet t2 = new AttributeSet();
+		
+		AttributeSet fd1 = new AttributeSet();
+		fd1.add(new Attribute("a"));
+		AttributeSet fd2 = new AttributeSet();
+		fd2.add(new Attribute("c"));
+		fd2.add(new Attribute("d"));
+		AttributeSet fd3 = new AttributeSet();
+		fd3.add(new Attribute("a"));
+		fd3.add(new Attribute("c"));
+		
+		Set<FunctionalDependency> fds = new HashSet<FunctionalDependency>();
+		
+		t1.add(new Attribute("a"));
+		t1.add(new Attribute("b"));
+		t1.add(new Attribute("d"));
+		t2.add(new Attribute("c"));
+		t2.add(new Attribute("e"));
+		t2.add(new Attribute("f"));
+
+		fds.add(new FunctionalDependency(fd1, new Attribute("b")));
+		fds.add(new FunctionalDependency(fd2, new Attribute("e")));
+		fds.add(new FunctionalDependency(fd3, new Attribute("f")));
+		
+		FDChecker.checkLossless(t1, t2, fds);
 	}
 
 }
